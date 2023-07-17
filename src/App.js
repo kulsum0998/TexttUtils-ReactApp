baf06daafca2def381bb4a52e45f54bb1b6e3382
kulsum0998 <83638819+kulsum0998@users.noreply.github.com>
2023-07-17 13:59:59 +0530

    TextUtils: A utility tool for manipulating and transforming text data efficiently and conveniently.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
-import React, { useState } from 'react';
 import Alert from './components/Alert';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link
 } from "react-router-dom";
 
- 
-function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
-  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
+function App() {
+  const[mode,setMode]= useState('light');
+  const[alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    });
+    setTimeout(()=>{
+      setAlert(null);
+    },3000);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode=()=>{
+    if(mode==='light')
+    {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success")
     }
   }
   return (
     <>
     <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-        <Routes>   
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert}/>} />
-        </Routes>
-      </div>
-    </Router>
-    </> 
+     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+     <Alert alert={alert}/>
+     <div className="container my-4">
+     <Routes>
+            <Route exact path="/about" element={<About />}>
+            </Route>
+            <Route exact path="/" element={<TextForm heading="TextUtils-Word Counter, Character Counter, Text Manipulation"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+          </Routes>
+      {/* <About/> */}
+     </div>
+     </Router>
+    </>
   );
 }
 
 export default App;
- 
